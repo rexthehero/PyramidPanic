461fc831b1363c2824d0cf97ab826eca6661b588
Arjan de Ruijter <adruijter@gmail.com>
2012-12-17 14:59:50 +0100

    De explorer kan nu weer terug het scherm inlopen als hij boven het scherm uit was gelopen. Ook het buiten het scherm tegen een muur lopen in de toestand down is opgelost. De explorer teleporteert niet meer in de muur.

--- PyramidPanic/PyramidPanic/PyramidPanic/GameScenes/PlayScene/Explorer/Down.cs
+++ PyramidPanic/PyramidPanic/PyramidPanic/GameScenes/PlayScene/Explorer/Down.cs
@@ -32,7 +32,9 @@ namespace PyramidPanic
             if (ExplorerManager.CollisionDetectionWalls())
             {
                 int geheelAantalmalen32 = (int)this.explorer.Position.Y / 32;
-                this.explorer.Position = new Vector2(this.explorer.Position.X, geheelAantalmalen32 * 32);
+                this.explorer.Position = (this.explorer.Position.Y >=0) ?
+                    new Vector2(this.explorer.Position.X, geheelAantalmalen32 * 32) :
+                    new Vector2(this.explorer.Position.X, (geheelAantalmalen32 - 1) * 32);
                 if (Input.DetectKeyUp(Keys.Down))
                 {
                     this.explorer.State = new Idle(this.explorer, (float)Math.PI / 2);
@@ -41,11 +43,15 @@ namespace PyramidPanic
             //Blijf op het grid
             if (Input.DetectKeyUp(Keys.Down))
             {
-                float modulo = this.explorer.Position.Y % 32;
+                float modulo = (this.explorer.Position.Y >= 0) ?
+                                this.explorer.Position.Y % 32 : 
+                                32 + this.explorer.Position.Y % 32;
                 if (modulo >= (32f - this.explorer.Speed))
                 {
                     int geheelAantalmalen32 = (int)this.explorer.Position.Y / 32;
-                    this.explorer.Position = new Vector2(this.explorer.Position.X, (geheelAantalmalen32 + 1) * 32);
+                    this.explorer.Position = (this.explorer.Position.Y >= 0) ?
+                        new Vector2(this.explorer.Position.X, (geheelAantalmalen32 + 1) * 32) :
+                        new Vector2(this.explorer.Position.X, (geheelAantalmalen32) * 32);
                     this.explorer.State = new Idle(this.explorer, (float)Math.PI/2);
                 }
             }
