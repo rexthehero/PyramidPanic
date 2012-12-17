b3f89009d0307532ba3267b13f696bf6bdd6d268
Arjan de Ruijter <adruijter@gmail.com>
2012-12-17 13:58:40 +0100

    De explorer kan nu buiten het spel vanaf rechts tegen een NotPassable object lopen n zondeen

--- PyramidPanic/PyramidPanic/PyramidPanic/GameScenes/PlayScene/Explorer/Right.cs
+++ PyramidPanic/PyramidPanic/PyramidPanic/GameScenes/PlayScene/Explorer/Right.cs
@@ -30,7 +30,9 @@ namespace PyramidPanic
             if (ExplorerManager.CollisionDetectionWalls())
             {
                 int geheelAantalmalen32 = (int)this.explorer.Position.X / 32;
-                this.explorer.Position = new Vector2(geheelAantalmalen32 * 32, this.explorer.Position.Y);
+                this.explorer.Position = (this.explorer.Position.X >= 0) ?
+                                          new Vector2((geheelAantalmalen32) * 32, this.explorer.Position.Y) :
+                                          new Vector2((geheelAantalmalen32 - 1) * 32, this.explorer.Position.Y);
                 if (Input.DetectKeyUp(Keys.Right))
                 {
                     this.explorer.State = new Idle(this.explorer, 0f);
@@ -44,7 +46,9 @@ namespace PyramidPanic
                 if (modulo >= (32f - this.explorer.Speed))
                 {
                     int geheelAantalmalen32 = (int)this.explorer.Position.X / 32;
-                    this.explorer.Position = new Vector2((geheelAantalmalen32 + 1) * 32, this.explorer.Position.Y);
+                    this.explorer.Position = (this.explorer.Position.X >= 0 ) ?
+                                              new Vector2((geheelAantalmalen32 + 1) * 32, this.explorer.Position.Y) :
+                                              new Vector2((geheelAantalmalen32) * 32, this.explorer.Position.Y);
                     this.explorer.State = new Idle(this.explorer, 0f);
                 }
             }
